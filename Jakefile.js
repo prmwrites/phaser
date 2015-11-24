a0b0f2855e190e036e19793015ecc7a731b466f1
prmwrites <prmwrites@gmail.com>
2015-11-24 19:52:06 +0000

    Setup simplebuild to run JSHint

--- Jakefile.js
+++ Jakefile.js
@@ -2,6 +2,7 @@
     "use strict";
     
     var semver = require("semver");
+    var jshint = require("simplebuild-jshint");
     
     desc("Default build");
     task("default", [ "version", "lint" ], function() {
@@ -23,9 +24,13 @@
     
     desc("Lint Javascript code");
     task("lint", function() {
-        console.log("Linting Javascript: .");
+        process.stdout.write("Linting Javascript: ");
         
-        jake.exec("node node_modules/jshint/bin/jshint Jakefile.js", { interactive: true }, complete);
-    });
+        jshint.checkFiles({
+            files: "Jakefile.js",
+            options: {},
+            globals: {}
+        }, complete, fail);
+    }, { async: true });
     
 }());
