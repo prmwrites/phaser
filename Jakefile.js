e5f83377a565895955b5855d1304e4df2a0eec0a
prmwrites <prmwrites@gmail.com>
2015-11-24 07:49:12 +0000

    npm installed semver

--- Jakefile.js
+++ Jakefile.js
@@ -1,8 +1,22 @@
 (function() {
     "use strict";
-    desc("");
-    task("default", function() {
+    
+    desc("Default build");
+    task("default", [ "version" ], function() {
         console.log("\nBUILD OK");    
+    });
+    
+    desc("Check Node version");
+    task("version", function() {
+        console.log("Checking Node version: .");
+        var packageJson = require("./package.json");
+        var expectedVersion = "v" + packageJson.engines.node;
+        
+        var actualVersion = process.version;
+        if (actualVersion !== expectedVersion) {
+            fail("Incorrect Node version: expected " + expectedVersion + ", but was " + actualVersion);
+        }
+        
     });
     
 }());
